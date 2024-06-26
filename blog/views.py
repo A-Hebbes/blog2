76994b4510d77405a00d1ef1663142c800fafbf7
Alexander Hebbes <aeh7@live.co.uk>
2024-06-26 06:56:22 +0000

    add homepage view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 # Create your views here.
 
-def my_blog(request):
-    return HttpResponse("Blog Started")
+class PostList(generic.ListView):
+    model = Post
+
