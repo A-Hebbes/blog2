150cf001927328bbbd9d579682829577356791a2
Alexander Hebbes <aeh7@live.co.uk>
2024-07-01 23:07:23 +0000

    continue upcoming posts

--- upcoming_posts/models.py
+++ upcoming_posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UpcomingPost(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="upcoming_posts")
+    status = models.IntegerField(default=0)
+    release_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- upcoming_posts/views.py
+++ upcoming_posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import UpcomingPost
 
 # Create your views here.
+
+def upcoming_view(request):
+    upcoming_post = UpcomingPost.objects.all().order_by('-release_date').first()
+
+return render(
+    request,
+    "upcoming_posts/upcoming.html",
+    {"upcoming_posts": upcoming_posts},
+)
+
