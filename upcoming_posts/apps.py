0216c9953b094bf4602bfefc88ac81376cb24e18
Alexander Hebbes <aeh7@live.co.uk>
2024-07-03 04:03:52 +0000

    Deleted upcoming_posts app and its files

--- upcoming_posts/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UpcomingPostsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'upcoming_posts'
--- upcoming_posts/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-#from .views import upcoming_view
-
-urlpatterns = [
-    path('upcoming/', upcoming_view, name='upcoming_posts'),
-    #path('', views.upcoming_posts, name='upcoming'),
-]
