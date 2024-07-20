556258a0f9f5735565ebd862cb44c899c427931c
Alexander Hebbes <aeh7@live.co.uk>
2024-07-20 11:24:20 +0000

    add url to comment edit

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('drafts/', views.draft_posts, name='draft_posts'),
     path('<slug:slug>/', views.post_full, name='post_full'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.edit_comment, name='edit_comment'),
     
 ]
