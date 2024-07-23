1151e3a26d64d21f653417f8ffc683536e3f796b
Alexander Hebbes <aeh7@live.co.uk>
2024-07-23 17:09:28 +0000

    begin modal layout

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('drafts/', views.draft_posts, name='draft_posts'),
     path('<slug:slug>/', views.post_full, name='post_full'),
     path('<slug:slug>/comment_edit/<int:comment_id>/', views.edit_comment, name='edit_comment'),
-    path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
+    path('<slug:slug>/delete_comment/<int:comment_id>', views.delete_comment, name='delete_comment'),
     
 ]
