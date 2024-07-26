01b7fcd8614f0e5eee79d28d74ae3948bb639b4a
Alexander Hebbes <aeh7@live.co.uk>
2024-07-26 09:54:22 +0000

    tidy variables

--- static/assets/js/comment.js
+++ static/assets/js/comment.js
@@ -1,47 +1,20 @@
-//Get Edit Buttons
-//document.addEventListener('DOMContentLoaded', function() {
-    
-
-//const editBtns = document.getElementsByClassName("edit-btn");
-//const commentInput = document.querySelector("#id_body");
-//const commentFormElement = document.querySelector("#commentForm");
-//const submitBtn = document.querySelector("#buttonSubmit");
-
-//for (let btn of editBtns) {
-//    btn.addEventListener("click", (event) => {
-//        let commentId = event.target.getAttribute("comment_id");
-//        let commentContent = document.getElementById(`comment${commentId}`).innerText;
-//        commentInput.value = commentContent.trim();
-//        submitBtn.innerText = "Update";
-//        commentFormElement.setAttribute("action", `/${post_slug}/comment_edit/${commentId}/`);
-//    });
-//}
-//});
-
 document.addEventListener('DOMContentLoaded', function() {
     console.log("DOM loaded");
-    const editBtns = document.getElementsByClassName("edit-btn");
-    console.log("Edit buttons found:", editBtns.length);
 
+    const editBtns = document.getElementsByClassName("edit-btn");
     const commentInput = document.querySelector("#id_body");
     const commentFormElement = document.querySelector("#commentForm");
     const submitBtn = document.querySelector("#buttonSubmit");
-
     const buttonsDelete = document.getElementsByClassName("delete-btn");
     const modalDelete = new bootstrap.Modal(document.getElementById("modalDelete"));
     const confirmDelete = document.getElementById("confirmDelete");
 
-
-
-    /*
-    FUnctions changed to try and fix del button 
-    const modalDelete = new bootstrap.Modal(document.getElementById("modalDelete"));
-    
-    const buttonsDelete = this.getElementsByClassName("delete-btn");
+    console.log("Edit buttons found:", editBtns.length);
     console.log("Delete buttons found:", buttonsDelete.length);
-    const confirmDelete = document.getElementById("confirmDelete");
-    */
 
+
+
+   
     for (let btn of editBtns) {
         btn.addEventListener("click", (event) => {
             console.log("Edit button clicked");
@@ -72,15 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
     
-/* DELETE COMMENT BEFORE TRY AT FIXING
-    for (let button of buttonsDelete) {
-        button.addEventListener("click", (event) =>
-        {let commentId = event.target.getAttribute("comment_id");
-            console.log("Delete Button Clicked, comment ID:", commentId);
-            confirmDelete.setAttribute = 'href', 'delete_comment/${commentId}';
-            modalDelete.show();
-    });
-    }*/
+
 
     
     for (let button of buttonsDelete) {
@@ -143,33 +108,3 @@ document.addEventListener('DOMContentLoaded', function() {
         return cookieValue;
     }
 });
-
-
-
-
-    /*document.addEventListener('DOMContentLoaded', function() {
-        const editBtns = document.getElementsByClassName("edit-btn");
-        const editCommentModal = new bootstrap.Modal(document.getElementById('editCommentModal'));
-        const editCommentForm = document.getElementById('editCommentForm');
-        const editCommentBody = document.getElementById('editCommentBody');
-        const editCommentId = document.getElementById('editCommentId');
-        const updateCommentBtn = document.getElementById('updateCommentBtn');
-    
-        for (let btn of editBtns) {
-            btn.addEventListener("click", (event) => {
-                let commentId = event.target.getAttribute("comment_id");
-                let commentContent = document.getElementById(`comment${commentId}`).innerText;
-                editCommentBody.value = commentContent.trim();
-                editCommentId.value = commentId;
-                editCommentModal.show();
-            });
-        }
-    
-        updateCommentBtn.addEventListener("click", () => {
-            let commentId = editCommentId.value;
-            let newAction = `/${post_slug}/comment_edit/${commentId}/`;
-            editCommentForm.setAttribute("action", newAction);
-            editCommentForm.submit();
-            editCommentModal.hide();
-        });
-    });*/
