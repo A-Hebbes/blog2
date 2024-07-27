780ac6e37c7ef9e02c0a3ceb8eeed66162402ca7
Alexander Hebbes <aeh7@live.co.uk>
2024-07-27 12:31:57 +0000

    add test for comment submission

--- blog/test_views.py
+++ blog/test_views.py
@@ -7,7 +7,7 @@ from .models import Post
 class BlogViewsTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_superuser(
-            usernsame = "testSuper",
+            username = "testSuper",
             password = "Password",
             email = "tester@testing.com"
         )
@@ -19,5 +19,24 @@ class BlogViewsTest(TestCase):
 
     def test_post_full_with_comment_form(self):
 
-    response = self.client.get(reverse('post_full', args=['test-blog-title']))
+        response = self.client.get(reverse('post_full', args=['test-blog-title']))
 
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Test Title", response.content)
+        self.assertIn(b"Test Blog Content", response.content)
+        self.assertIsInstance(response.context['comment_form'], CommentForm)
+
+    def test_successful_comment_submission(self):
+        self.client.login(
+            username="testSuper",
+            password="Password",
+        )
+
+        post_data = {
+    'body': 'Comment for test.'
+}
+
+response = self.client.post(reverse(
+    'post_full', args=['blog']), post_data)
+
+    self.assertEqual(response.status_code, 200)
