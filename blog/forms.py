57a3448a1bddde65b5014210cc577a9d7717d1fa
Alexander Hebbes <aeh7@live.co.uk>
2024-10-27 00:00:50 +0000

    Add PostForm and create edit_post.html

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from .models import Comment
+from .models import Comment, Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'excerpt', 'status']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 10}),
+            'excerpt': forms.Textarea(attrs={'rows': 3}),
+        }
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+
