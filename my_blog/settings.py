ca4043b93a8597558451b1e40c37df096438f13d
Alexander Hebbes <aeh7@live.co.uk>
2024-11-07 22:45:26 +0000

    Fix: Move database url and secret key to env.py

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -26,8 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ('django-insecure-xmgx@t=eze*b(gylbqz1m^vj'
-              '(ybm3^6m@0djy*1af#r7m+5gcl')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
